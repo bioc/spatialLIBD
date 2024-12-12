0474fc71e564d65e63451b6822cfdad009bfab33
lcolladotor <lcolladotor@gmail.com>
2024-12-12 15:44:58 -0500

    Resolve an R CMD CHECK note from https://github.com/LieberInstitute/spatialLIBD/actions/runs/12303813508/job/34339808466#step:20:82

--- R/app_server.R
+++ R/app_server.R
@@ -1488,7 +1488,7 @@ app_server <- function(input, output, session) {
                 clusters = rownames(static_layer_external_tstat())
             ),
             reference_colors = get_colors(
-                libd_layer_colors,
+                spatialLIBD::libd_layer_colors,
                 clusters = colnames(static_layer_external_tstat())
             )
         )
